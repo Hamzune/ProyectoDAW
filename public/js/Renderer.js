51fe3adab82e083f71e8a45297f8b0553da3b18b
Soufiane aatab <soufianiku@gmail.com>
2018-04-25 11:51:59 +0200

    clase renderer creada

---
+++ public/js/Renderer.js
@@ -0,0 +1,15 @@
+function Renderer(game){
+    this.game = game;
+    
+    this.create = function(){
+
+    }
+
+    this.update = function() {
+
+    }
+
+    this.preload = function() {
+
+    }
+}
