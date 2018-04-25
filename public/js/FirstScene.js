ed94c882eac294b0738f32d5285e07959ae535fa
server <hamza.ben.deljulia@gmail.com>
2018-04-25 13:04:11 +0200

    probando el player

--- public/js/FirstScene.js
+++ public/js/FirstScene.js
@@ -1,41 +1,45 @@
-function FirstScene(game){
+function FirstScene(game) {
     this.game = game;
-
     this.preload = function () {
-        this.game.load.image('ship', 'assets/ship.png');
+        this.player = new Player(this.game);
+        this.player.preload();
 
     }
 
-    this.create = function() {
+    this.create = function () {
         this.game.stage.backgroundColor = "#ff5900";
-        var image = this.game.add.sprite(this.game.world.centerX, this.game.world.centerY, 'ship');
+        this.player.create(100, 100);
 
-        //  Moves the image anchor to the middle, so it centers inside the game properly
-        image.anchor.set(0.5);
-    
-        //  Enables all kind of input actions on this image (click, etc)
-        image.inputEnabled = true;
-    
-        text = this.game.add.text(250, 16, '', { fill: '#ffffff' });
-    
-        image.events.onInputDown.add(this.listener, this);
-
+        game.physics.arcade.enable(this.player.getSprite());
     }
 
-    this.listener = function(){
+    this.listener = function () {
 
         this.game.myrenderer.changeScene(new SecondScene(this.game));
     }
 
-    this.update = function() {
+    this.update = function () {
 
+        this.player.setVelocityX(0);
+        this.player.setVelocityY(0);
+        this.player.getSprite().body.angularVelocity = 0;
+
+        this.player.movement();
+
+        // La apunta al mouse, siempre
+        var dy = this.game.input.mousePointer.y - this.player.getSprite().y;
+        var dx = this.game.input.mousePointer.x - this.player.getSprite().x;
+    
+        var angle = Math.atan2(dy, dx);
+    
+        this.player.getSprite().rotation = angle;
     }
 
     this.render = function () {
 
     }
 
-    this.stop = function() {
+    this.stop = function () {
         this.game.world.removeAll();
     }
 }
