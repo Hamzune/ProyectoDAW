0d1d8d43df78049de34052e16adf55c233ce2777
Soufiane aatab <soufianiku@gmail.com>
2018-05-14 12:36:03 +0200

    fixed window resizing

--- public/js/game.js
+++ public/js/game.js
@@ -1,6 +1,17 @@
 var game = new Phaser.Game(window.innerWidth, window.innerHeight, Phaser.CANVAS, 'game');
 var renderer = new Renderer(game);
 
+
+// Fixed resize window
+window.onresize = function(){
+    var canvas = document.getElementsByTagName('canvas')[0];
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+    canvas.style.width = window.innerWidth + "px";
+    canvas.style.height = window.inneHeight + "px";
+    game.scale.setGameSize(window.innerWidth, window.innerHeight);
+}
+
 game.myrenderer = renderer;
 
 game.state.add('game', renderer);
